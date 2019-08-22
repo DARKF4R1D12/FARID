77fcdda38be2cad66ff20f3732a4aa5c4fa2fdfa
DARKF4R1D12 <54403042+DARKF4R1D12@users.noreply.github.com>
2019-08-22 19:38:36 +0700

    Add files via upload

---
+++ TARGET.py
@@ -0,0 +1,212 @@
+44.sh
+@@ -1,213 +0,0 @@
+#! / usr / bin / bash
+bersih
+# ini warna
+bi = ' \ 033 [34; 1m '  # biru
+i = ' \ 033 [32; 1m '  # ijo
+pur = ' \ 033 [35; 1m '  # ungu
+cy = ' \ 033 [36; 1m '  # cyan
+saya = ' \ 033 [31; 1m '  # merah
+pu = ' \ 033 [37; 1m '  # putih
+ku = ' \ 033 [33; 1m '  # kuning
+# AC1LDAY127
+gema
+gema
+# paket ini untuk menjalankan tools
+t.sh
+gema
+bersih
+gema
+echo  $ me "        ,; ~ '' ~ ;, "
+echo  $ me "       ,;;, "
+echo  $ me "     ;; "
+echo  $ me "    , '', "
+echo  $ me "   ,;;, "
+menggemakan  $ me "   ;;..;; "
+echo  $ me "   |; ______ ______; | "
+echo  $ me "   | '/ ~' ~ '.' ~ '~ \' | "
+echo  $ me "   | ~, - ~~~ ^ ~, |, ~ ^ ~~~ -, ~ | "
+echo  $ me "    | " $ me "    |}: {| | "
+echo  $ me "    | " $ me "    l / | \! | "
+echo  $ me "    . ~ " $ me "   (__, .-- '. ^.' -., __) ~. "
+echo  $ cy "    | ---; ' / | \ '; --- | "
+echo  $ cy "      \ __. \ / ^ \ / .__ / "
+echo  $ cy "     V | \ / | V "
+echo  $ ku "       | | T ~ \ ___! ___! ___ / ~ T | | "
+echo  $ ku "       | | 'IIII_I_I_I_IIII' | | "
+echo  $ ku "       | \, III III III, / | "
+echo  $ cy "         \ '~~~~~~~~~~' / "
+echo  $ cy "          \. / "
+echo  $ cy "           \. ^ ./ "
+echo  $ cy "             ^ ~~~ ^ ~~~ ^ "
+tidur 6
+gema
+echo  $ cy " [✓] MERAHPUTIH "
+echo  $ me " ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ " $ pu " █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ "
+echo  $ me  " AUTHOR: AC1L D4Y "
+echo  $ i   " WA: 085280645068 "
+echo  $ i   " YOUTUBE: MR 4444 "
+echo  $ i   " Versi: Alat ini Versi 1.2 "
+echo  $ pu  " CATATAN: JIKA ADA MASALAH HUBUNGI PEMBUAT SCRIP "
+echo  $ pu " ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ " $ saya " █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ "
+tidur 4
+gema
+gema  $ pu "                ╭∩╮ （￣ ▽ ￣） ╭∩╮ "
+echo  $ me " ║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌ "
+echo  $ pu "      " $ me "    [•] [1] HACK FB     " $ me "   [•]       "
+echo  $ pu "      " $ ku "    [•] [2] HACK GMAIL   " $ ku "   [•]     "
+echo  $ pu "      " $ i "   [•] [3] HACK IG      " $ i "   [•]     "
+echo  $ pu "      " $ bi "    [•] [4] DEFACEEE     " $ bi "   [•]     "
+echo  $ pu " ║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌ "
+echo  $ cy " ┌ === " $ bi " [ACIL D4YY] "
+echo  $ cy " ¦ "
+baca -p " >>> " pil ;
+
+ if [ $ pil  = 1]
+kemudian
+bersih
+echo  $ me " ╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔═══╗╔═══╗╔═══╗╔═══╗╔══╗─╔ ═══╗╔═══╗╔╗╔═╗ "
+echo  $ me " ║║─║║║╔═╗║║╔═╗║║║║╔╝║╔══╝║╔═╗║║╔═╗║║╔══╝║╔╗║─║ ╔═╗║║╔═╗║║║║╔╝ "
+echo  $ me " ║╚═╝║║║─║║║║─╚╝║╚╝╝─║╚══╗║║─║║║║─╚╝║╚══╗║╚╝╚╗║ ║─║║║║─║║║╚╝╝─ "
+echo  $ pu " ║╔═╗║║╚═╝║║║─╔╗║╔╗║─║╔══╝║╚═╝║║║─╔╗║╔══╝║╔═╗║║ ║─║║║║─║║║╔╗║─ "
+echo  $ pu " ║║─║║║╔═╗║║╚═╝║║║║╚╗║║───║╔═╗║║╚═╝║║╚══╗║╚═╝║║ ╚═╝║║╚═╝║║║║╚╗ "
+echo  $ pu " ╚╝─╚╝╚╝─╚╝╚═══╝╚╝╚═╝╚╝───╚╝─╚╝╚═══╝╚═══╝╚═══╝╚ ═══╝╚═══╝╚╝╚═╝- "
+tidur 4
+echo  $ me " ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ "
+echo  $ i      " Penulis: ACIL D4Y 127 "
+echo  $ i      " WA: 085280645068 "
+echo  $ i      " Tools: Untuk Hack Facebook "
+echo  $ pu " ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■■ █ ■ █ ■ █ ■ █ ■ █ ■ █ ■ █ "
+tidur 4
+gema
+baca -p " minta nama Facebook Korban: " script ;
+gema  " Memuat ..... "  | lolcat
+tidur 4
+echo  " Membuka Pengaturan facebook "  | lolcat
+gema  " Memuat ..... "
+tidur 4
+echo  " Berhasil mencury Data korban "  | lolcat
+echo  " ============================== " "
+echo  $ me " Email / HP: Tidak ditemukan !!
+echo $ cy " Kata Sandi : apaajah2838 "
+echo " ============================== " "
+echo $ me " CATATAN : JIKA ADA MASALAH HUBUNGI AUTHOR "
+echo $ i " Terimakasih telah menggunakan tools ini "
+fi
+ if [ $ pil = 2]
+kemudian
+bersih
+hapus # ini warna
+bi = '\ 033 [34; 1m' #biru
+i = '\ 033 [32; 1m' #ijo
+pur = '\ 033 [35; 1m' #purple
+cy = '\ 033 [36; 1m' #cyan
+saya = '\ 033 [31; 1m' #merah
+pu = '\ 033 [37; 1m' #putih
+ku = '\ 033 [33; 1m' #kuning
+# AC1LDAY127
+gema
+gema
+#ini paket untuk menjalankan tools
+t.sh
+gema
+bersih
+gema
+gema
+echo $ me " █░░ ▄▀▄ ▄▀ █░▄▀ ▄▀▀░ █▄░▄█ ▄▀▄ ▀ █░░ "
+echo $ me " █▀▄ █▀█ █░ █▀▄░ █░▀▌ █░█░█ █▀█ █ █░▄ "
+gema $ pu " ▀░▀ ▀░▀ ░▀ ▀░▀▀ ▀▀▀░ ▀░░░▀ ▀░▀ ▀ ▀▀▀ "
+echo $ pu  " ========================= "
+echo $ i   " PENULIS : ACIL D4Y 127 "
+echo $ cy  " WA : + 62852-8064-5068 "
+echo $ i   " Youtube : MR 4444 "
+echo $ me  " ========================= "
+echo " Selamat Datang Di Alat Peretas Gmail "
+gema " Memuat ..... " | lolcat
+baca -p " larang nama pengguna GMAIL korban :  " script;
+tidur 4
+echo " Akun gmail Sedang di retas Harap TUNGGU ! " | lolcat
+tidur 4
+tidur 2
+echo " Akun berhasil di retas "
+echo " ======================== "
+echo $ i  " Kata Sandi : orangganteng992 "
+echo " ======================== "
+echo $ me  " CATATAN JIKA ADA MASALAH HUBUNGI PENULIS "
+echo " Terimakasih telah menggunakan tools kami :)) " | lolcat
+fi
+ if [ $ pil = 3]
+kemudian
+bersih
+hapus # ini warna
+bi = '\ 033 [34; 1m' #biru
+i = '\ 033 [32; 1m' #ijo
+pur = '\ 033 [35; 1m' #purple
+cy = '\ 033 [36; 1m' #cyan
+saya = '\ 033 [31; 1m' #merah
+pu = '\ 033 [37; 1m' #putih
+ku = '\ 033 [33; 1m' #kuning
+# AC1LDAY127
+gema
+gema
+#ini paket untuk menjalankan tools
+t.sh
+gema
+bersih
+gema
+gema
+echo $ me " █── ▄▀▄ ▄▀ █─▄▀ ▀ █▄─█ ▄▀▀ ▀█▀ ▄▀▄ ▄▀▀ ▄▀▀─ █▀▀▄ ▄▀▄ █▄─▄█ "
+echo $ i  " █▀▄ █▀█ █─ █▀▄─ █ █─▀█ ─▀▄ ─█─ █▀█ █─▀▌ █▐█▀ █▀█ █─█─█ "
+echo $ me " ▀─▀ ▀─▀ ─▀ ▀─▀▀ ▀ ▀──▀ ▀▀─ ─▀─ ▀─▀ ▀▀▀─ ▀─▀▀ ▀─▀ ▀───▀ "
+echo $ i  " ==================== " "
+echo $ cy " Penulis : ACIL D4Y 127 "
+echo $ cy " Youtube : MR 4444 "
+echo $ i  " ==================== " "
+echo " Selamat Datang Di Tools Hack instagram "
+gema " Memuat .... "
+tidur 4
+baca -p " letakkan nama pengguna / nama instagram :  " script;
+gema " Memuat ...... "
+tidur 4
+echo " Akun instagram Sedang di retas Harap TUNGGU ! " | lolcat
+tidur 4
+tidur 2
+echo " Akun berhasil di retas "
+tidur 4
+echo " ======================== "
+echo $ me  " Kata Sandi : carisendiria3637 "
+echo " ======================== "
+echo $ me  " CATATAN JIKA ADA MASALAH HUBUNGI PENULIS "
+echo " Terimakasih gan RESIKO DI TANGGUNG SENDIRI " | lolcat
+fi
+ if [ $ pil = 4]
+kemudian
+bersih
+hapus # ini warna
+bi = '\ 033 [34; 1m' #biru
+i = '\ 033 [32; 1m' #ijo
+pur = '\ 033 [35; 1m' #purple
+cy = '\ 033 [36; 1m' #cyan
+saya = '\ 033 [31; 1m' #merah
+pu = '\ 033 [37; 1m' #putih
+ku = '\ 033 [33; 1m' #kuning
+# AC1LDAY127
+gema
+gema
+#ini paket untuk menjalankan tools
+t.sh
+gema
+bersih
+gema
+gema
+echo $ pur " ──╔╗──────────────────────────╔╗── "
+echo $ pur " ──║║──────────────────────────║║── "
+echo $ me  " ╔═╝║╔══╗╔╝╚╗╔══╗╔══╗╔══╗╔╗╔╗╔╗╔══╗║╚═╗ "
+echo $ me  " ║╔╗║║║═╣╚╗╔╝║╔╗║║╔═╝║║═╣║╚╝╚╝║║║═╣║╔╗║ "
+echo $ cy  " ║╚╝║║║═╣─║║─║╔╗║║╚═╗║║═╣╚╗╔╗╔╝║║═╣║╚╝║ "
+echo $ cy  " ╚══╝╚══╝─╚╝─╚╝╚╝╚══╝╚══╝─╚╝╚╝─╚══╝╚══╝ "
+echo $ pur " ______________________________________ "
+gema " "
+echo $ pu  " ALAT MAAF DEFACEWEB SEDANG UPDATE :) "
+fi
